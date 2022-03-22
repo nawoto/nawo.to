0ddecabadc0a2701e38bc683dbf628413fef7468
Naoto Nishimura <nishimura@nawo.to>
2022-03-22 14:15:45 +0900

    remove unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,9 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
 import Header from "../components/header"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="font-Titillium font-black text-4xl tracking-widest">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
-  const menu = (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Top</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  )
-
 
   return (
     <div className="" data-is-root-path={isRootPath}>
