6e7495cee39e431ccadec4e6c520d146c0bc13dc
nawoto <nishimura@nawo.to>
2025-06-09 22:45:02 +0900

    fix: config.tsファイル作成とインポートパス修正 - Cannot read properties of undefined エラーを解決

---
+++ nawo.to-astro/src/config.ts
@@ -0,0 +1,12 @@
+export const SITE = {
+  title: '#nawoto',
+  description: '@nawoto write some texts about Software Development, Life and more',
+  url: 'https://nawo.to',
+  author: {
+    name: 'NISHIMURA Naoto',
+    summary: '西村直人。おもに nawoto という名前でいろいろしています',
+  },
+  social: {
+    twitter: 'nawoto',
+  },
+} as const; 
