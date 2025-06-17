e1593b0c7051f3c43a0a3c0ac7a52d9a922d8ffd
nawoto <nishimura@nawo.to>
2025-06-17 15:27:24 +0900

    feat: migrate from Gatsby to Astro

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,6 @@
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwind,
-    autoprefixer,
-  ],
-}; 
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+} 
